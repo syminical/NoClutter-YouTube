72efcdd1da25aa1b25ffde440630216fc668344a
syminical <syminical@gmail.com>
2020-06-08 17:48:02 -0400

    works?, new searches in YT not filtered

--- noClarifyYT.js
+++ noClarifyYT.js
@@ -1,51 +1,59 @@
 console.log(new Date());
 
-purgeS = function(id) {
-  browser.tabs.executeScript(id, {code:`
+const purgeS = `
     evils = document.getElementsByTagName("ytd-clarification-renderer");
     if (evils.length > 0) {
-      for (i = 0; i < evils.length; ++i)
+      for (i = 0; i < evils.length; ++i) {
         evils[i].remove();
+        if (typeof guardian !== "undefined") {
+          guardian.disconnect();
+          delete guardian;
+        }
+      }
     }
     evils = document.getElementsByTagName("ytd-info-panel-container-renderer");
     if (evils.length > 0) {
-      for (i = 0; i < evils.length; ++i)
+      for (i = 0; i < evils.length; ++i) {
         evils[i].remove();
+        if (typeof guardian !== "undefined") {
+          guardian.disconnect();
+          delete guardian;
+        }
+      }
     }
     delete evils;`
-  });
-}
 
-purgeV = function(id) {
-  browser.tabs.executeScript(id, {code:`
+const purgeV = `
     evil = document.getElementById("clarify-box");
     if (evil !== null) {
       evil.parentNode.removeChild(evil);
-    } else { console.log("no clarif box"); }
+      if (typeof guardian !== "undefined") {
+        guardian.disconnect();
+        delete guardian;
+      }
+    }
     delete evil;`
-  });
+
+protect = function(wpn, id) {
+  browser.tabs.executeScript(id, {code:`
+    wpn = \`${wpn}\`;
+    guardian = new MutationObserver(function(g=guardian, w=wpn) {
+      eval(w);
+      g.disconnect();
+      delete g;
+    });
+    guardian.observe(document.getElementsByTagName("body")[0], {childList: true, subtree: true});
+    eval(wpn);`});
 }
 
 browser.tabs.onUpdated.addListener(function(id, change) {
-  console.log("SEARCH");
-  if (change.url)
-    purgeS(id);
-}, {urls:["*://*.youtube.com/results?*"]});
-
-browser.tabs.onUpdated.addListener(function(id, change) {
-  console.log("SEARCH");
-  if (change.status == "complete")
-    purgeS(id);
+  if (change.status == "complete") {
+    protect(purgeS, id);
+  }
 }, {urls:["*://*.youtube.com/results?*"], properties:["status"]});
 
 browser.tabs.onUpdated.addListener(function(id, change) {
-  console.log("VIDEO");
-  if (change.url)
-    purgeV(id);
-}, {urls:["*://*.youtube.com/watch?*"]});
-
-browser.tabs.onUpdated.addListener(function(id, change) {
-  console.log("VIDEO");
-  if (change.status == "complete")
-    purgeV(id);
+  if (change.status == "complete") {
+    protect(purgeV, id);
+  }
 }, {urls:["*://*.youtube.com/watch?*"], properties:["status"]});

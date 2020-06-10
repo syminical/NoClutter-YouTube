122b8c289c4acb75487a58f7977ff57af9833d92
syminical <syminical@gmail.com>
2020-06-10 13:49:13 -0400

    observer is not removed now after deleting the box

--- noClarifyYT.js
+++ noClarifyYT.js
@@ -5,20 +5,20 @@ const purgeS = `
     if (evils.length > 0) {
       for (i = 0; i < evils.length; ++i) {
         evils[i].remove();
-        if (typeof guardian !== "undefined") {
+        /*if (typeof guardian !== "undefined") {
           guardian.disconnect();
           delete guardian;
-        }
+        }*/
       }
     }
     evils = document.getElementsByTagName("ytd-info-panel-container-renderer");
     if (evils.length > 0) {
       for (i = 0; i < evils.length; ++i) {
         evils[i].remove();
-        if (typeof guardian !== "undefined") {
+        /*if (typeof guardian !== "undefined") {
           guardian.disconnect();
           delete guardian;
-        }
+        }*/
       }
     }
     delete evils;`
@@ -27,10 +27,10 @@ const purgeV = `
     evil = document.getElementById("clarify-box");
     if (evil !== null) {
       evil.parentNode.removeChild(evil);
-      if (typeof guardian !== "undefined") {
+      /*if (typeof guardian !== "undefined") {
         guardian.disconnect();
         delete guardian;
-      }
+      }*/
     }
     delete evil;`
 
@@ -39,8 +39,8 @@ protect = function(wpn, id) {
     wpn = \`${wpn}\`;
     guardian = new MutationObserver(function(g=guardian, w=wpn) {
       eval(w);
-      g.disconnect();
-      delete g;
+      //g.disconnect();
+      //delete g;
     });
     guardian.observe(document.getElementsByTagName("body")[0], {childList: true, subtree: true});
     eval(wpn);`});
